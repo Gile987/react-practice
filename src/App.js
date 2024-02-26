0b0117d5424c240127c420ed5d21b0f87f3c173c
Gile <mladen.reljic87@gmail.com>
2024-02-26 09:32:30 +0100

    add a sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
-import { ThemeProvider } from '@emotion/react';
-import theme from './theme';
+import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
+import Sidebar from "./components/Sidebar";
 
 function App() {
+  const components = ["Component1", "Component2", "Component3"];
+
+  const handleSelectComponent = (component) => {
+    console.log("Selected component:", component);
+  };
+
   return (
     <ThemeProvider theme={theme}>
-
+      <div className="App">
+        <Sidebar
+          components={components}
+          onSelectComponent={handleSelectComponent}
+        />
+      </div>
     </ThemeProvider>
   );
 }
